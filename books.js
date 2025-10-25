b87b2b230f1305c5b3fc1bcb98abcb22c6b938f9
tea-eraspillsthecode. <tierrageorge01@gmail.com>
2025-10-24 21:37:08 -0400

    used 'or' operator to sort book prices based on whether there is a sale

--- books.js
+++ books.js
@@ -3,9 +3,9 @@ function renderBooks(filter) {
   const books = getBooks();
 
   if (filter === "LOW_TO_HIGH") {
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
+    books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice))
   } else if (filter === "HIGH_TO_LOW") {
-    books.sort((a, b) => b.originalPrice - a.originalPrice);
+    books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice))
   } else if (filter === "RATING") {
     books.sort((a, b) => b.rating - a.rating);
   }
